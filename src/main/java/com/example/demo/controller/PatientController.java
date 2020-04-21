19a1a8eb6ee161173c8d71c136cb7c34d01f195c
regret <951489398@qq.com>
2020-04-21 23:13:32 +0800

    完成患者主治医生信息显示功能

--- src/main/java/com/example/demo/controller/PatientController.java
+++ src/main/java/com/example/demo/controller/PatientController.java
@@ -1,8 +1,40 @@
 package com.example.demo.controller;
 
+import com.example.demo.entities.Doctor;
+import com.example.demo.entities.Patient;
+import com.example.demo.repository.DoctorRepository;
+import com.example.demo.repository.PatientRepository;
+import com.example.demo.repository.SickroomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class PatientController {
+    @Autowired
+    PatientRepository patientRepository;
+
+    @Autowired
+    SickroomRepository sickroomRepository;
+
+    @Autowired
+    DoctorRepository doctorRepository;
+
+
+    /**
+     * 查询我的医生
+     * @param id
+     * @return
+     */
+    @GetMapping(value = "/myDoctor/{id}")
+    public String myDoctor(@PathVariable("id") Integer id,
+                           Model model){
+        Patient patient = patientRepository.findById(id).orElse(null);
+        Doctor doctor = patient.getDoctor();
+        model.addAttribute("mydoc",doctor);
+        return "JDBCForPatient/listforMyDoctor";
+    }
 
 }
