e55c0c580211a27ce6d454db429ffd76b9cdd477
regret <951489398@qq.com>
2020-05-15 18:05:10 +0800

    完成患者病历查看功能

--- src/main/java/com/example/demo/controller/MedicalCardController.java
+++ src/main/java/com/example/demo/controller/MedicalCardController.java
@@ -1,14 +1,34 @@
 package com.example.demo.controller;
 
+import com.example.demo.entities.TblCard;
+import com.example.demo.repository.PatientRepository;
+import com.example.demo.repository.TblRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class MedicalCardController {
 
+    @Autowired
+    PatientRepository patientRepository;
+    @Autowired
+    TblRepository tblRepository;
+
+    /**
+     *
+     * @param id 患者id
+     * @param model
+     * @return
+     */
     @GetMapping(value = "/MedicalCardForPatient/{id}")
-    public String MedicalCardForPatient(@PathVariable("id") Integer id){
+    public String MedicalCardForPatient(@PathVariable("id") Integer id,
+                                        Model model){
+        TblCard tblCard = tblRepository.findByPatient(patientRepository.findById(id).orElse(null));
+        model.addAttribute("tbl",tblCard);
+
         return "JDBCForPatient/MedicalCard";
 
     }
--- src/main/java/com/example/demo/repository/TblRepository.java
+++ src/main/java/com/example/demo/repository/TblRepository.java
@@ -1,7 +1,9 @@
 package com.example.demo.repository;
 
+import com.example.demo.entities.Patient;
 import com.example.demo.entities.TblCard;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TblRepository extends JpaRepository<TblCard,Integer> {
+    TblCard findByPatient(Patient patient);
 }
